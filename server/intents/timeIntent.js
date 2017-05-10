de70f7c60c0f16438a9ebcf72732cc8e20004a98
Danjelous <daniel.brunner@live.at>
2017-05-10 14:45:48 +0200

    Complete geocoding and time services with google apis

--- server/intents/timeIntent.js
+++ server/intents/timeIntent.js
@@ -4,13 +4,20 @@ const request = require('superagent');
 
 module.exports.process = function process(intentData, log, cb) {
 
-    if(intentData.intent[0].value !== 'time') {
+    if (intentData.intent[0].value !== 'time') {
 
         // Give back the callback with an error object
         return cb(new Error('Expected time intent but got ' + intentData.intent[0].value));
     }
-    const location = intentData.location[0].value;
 
-    // Error is null --> err, res
-    return cb(null, `In ${location} it is now NO IDEA.`);
+    const location = intentData.location[0].value;
+    request.get('http://localhost:3001/service/' + location)
+        .then((res) => {
+
+            if (!res.body.result) return cb('Error with time service');
+
+
+            // Error is null --> err, res
+            return cb(null, `In ${location} it is now ${res.body.result}.`);
+        })
 }
